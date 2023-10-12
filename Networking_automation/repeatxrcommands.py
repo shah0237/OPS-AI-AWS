ab3c5329e7445bc7c371a939b98fea389f2c09cb
shah0237 <147306591+shah0237@users.noreply.github.com>
2023-10-12 17:44:47 -0400

    Update repeatxrcommands.py

--- Networking_automation/repeatxrcommands.py
+++ Networking_automation/repeatxrcommands.py
@@ -1,1 +1,2 @@
+#Built script while at cisco, presented to network engineer so they can simulate link flapping by turning port on and off at set interval timers, will repeat any xr command given in commands.txt file
 
